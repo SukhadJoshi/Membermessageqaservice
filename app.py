c999f2ef3e6220dd41f18b160650f9a54e286d58
SukhadJoshi <sukhad2002@gmail.com>
2025-11-14 11:09:31 -0500

    Commit app, requirements, and ignore files

--- app.py
+++ app.py
@@ -12,6 +12,17 @@ app = FastAPI(
     version="1.0.0",
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 async def fetch_messages():
     async with httpx.AsyncClient(timeout=10.0) as client:
